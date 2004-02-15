0d0944099a9d8dc1e2717d9a69dc203d6f42c6ec
tfheen <tfheen@153bf9da-15d3-0310-89f8-c842cf69dc3c>
2004-02-15 19:13:14 +0000

    import
    git-svn-id: svn://anonscm.debian.org/pkg-mailman/trunk@2 153bf9da-15d3-0310-89f8-c842cf69dc3c

---
+++ debian/mm_cfg.py
@@ -0,0 +1,62 @@
+# -*- python -*-
+
+# Copyright (C) 1998,1999,2000 by the Free Software Foundation, Inc.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software 
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+
+"""This is the module which takes your site-specific settings.
+
+From a raw distribution it should be copied to mm_cfg.py.  If you
+already have an mm_cfg.py, be careful to add in only the new settings
+you want.  The complete set of distributed defaults, with annotation,
+are in ./Defaults.  In mm_cfg, override only those you want to
+change, after the
+
+  from Defaults import *
+
+line (see below).
+
+Note that these are just default settings - many can be overridden via the
+admin and user interfaces on a per-list or per-user basis.
+
+Note also that some of the settings are resolved against the active list
+setting by using the value as a format string against the
+list-instance-object's dictionary - see the distributed value of
+DEFAULT_MSG_FOOTER for an example."""
+
+
+#######################################################
+#    Here's where we get the distributed defaults.    #
+
+from Defaults import *
+
+##############################################################
+# Put YOUR site-specific configuration below, in mm_cfg.py . #
+# See Defaults.py for explanations of the values.	     #
+
+DEFAULT_URL       = 'http://thunderchild.aszi.sztaki.hu/cgi-bin/mailman/'
+DEFAULT_EMAIL_HOST = 'thunderchild.aszi.sztaki.hu'
+DEFAULT_URL_HOST = 'thunderchild.aszi.sztaki.hu'
+IMAGE_LOGOS       = '/images/mailman/'
+USE_ENVELOPE_SENDER = 0
+DEFAULT_SEND_REMINDERS = 0
+
+MAILMAN_OWNER     = 'mailman-owner@%s' % DEFAULT_HOST_NAME
+
+PRIVATE_ARCHIVE_URL = '/cgi-bin/mailman/private'
+
+# Note - if you're looking for something that is imported from mm_cfg, but you
+# didn't find it above, it's probably in Defaults.py.
