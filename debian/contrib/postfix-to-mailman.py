8ae1bf93a938f0453d1861b1b9c8edd6ac3b3b80
thijs <thijs@153bf9da-15d3-0310-89f8-c842cf69dc3c>
2009-12-29 19:36:58 +0000

    Recognise -loop suffix in postfix-to-mailman.py (closes: #562700).
    git-svn-id: svn://anonscm.debian.org/pkg-mailman/trunk@640 153bf9da-15d3-0310-89f8-c842cf69dc3c

--- debian/contrib/postfix-to-mailman.py
+++ debian/contrib/postfix-to-mailman.py
@@ -129,6 +129,7 @@ def main():
                 '-confirm',
                 '-join',
                 '-leave',
+                '-loop',
                 '-subscribe',
                 '-unsubscribe',
                 ):
