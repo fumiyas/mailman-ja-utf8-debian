626f6f2220e38f223d8b3609ec4aba4cb9649a35
thijs <thijs@153bf9da-15d3-0310-89f8-c842cf69dc3c>
2013-08-04 09:59:46 +0000

    Import new upstream release candidate 2.1.16 rc2
    git-svn-id: svn://anonscm.debian.org/pkg-mailman/trunk@734 153bf9da-15d3-0310-89f8-c842cf69dc3c

--- Mailman/Gui/General.py
+++ Mailman/Gui/General.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2001-2011 by the Free Software Foundation, Inc.
+# Copyright (C) 2001-2013 by the Free Software Foundation, Inc.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -153,7 +153,21 @@ class General(GUIBase):
              directive. eg.; [listname %%d] -> [listname 123]
                             (listname %%05d) -> (listname 00123)
              """)),
+          ]
 
+        if mm_cfg.ALLOW_AUTHOR_IS_LIST:
+            rtn.append(
+                ('author_is_list', mm_cfg.Radio, (_('No'), _('Yes')), 0,
+                 _("""Replace the sender with the list address to conform with
+                 policies like ADSP and DMARC.  It replaces the poster's
+                 address in the From: header with the list address and adds the
+                 poster to the Reply-To: header, but the anonymous_list and
+                 Reply-To: header munging settings below take priority.  If
+                 setting this to Yes, it is advised to set the MTA to DKIM sign
+                 all emails."""))
+              )
+
+        rtn.extend([
             ('anonymous_list', mm_cfg.Radio, (_('No'), _('Yes')), 0,
              _("""Hide the sender of a message, replacing it with the list
              address (Removes From, Sender and Reply-To fields)""")),
@@ -374,7 +388,7 @@ class General(GUIBase):
              useful for selecting among alternative names of a host that has
              multiple addresses.""")),
 
-          ]
+          ])
 
         if mm_cfg.ALLOW_RFC2369_OVERRIDES:
             rtn.append(
